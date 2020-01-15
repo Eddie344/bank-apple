0d7e12d48b1d9a4d3bb285289973951b0a5c2fd5
Eddie <53879350+Eddie344@users.noreply.github.com>
2020-01-15 20:10:37 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
 	var mySwiper = new Swiper ('.swiper-container', {
     // Optional parameters
 		loop: false,
+		slidesPerView: 'auto',
 		breakpoints: {
-			500: {
-				slidesPerView: 2,
-				spaceBetween: 20,
-			},
-			768: {
-				slidesPerView: 3,
-			},
 			1023: {
 				noSwiping: false,
 				allowSlidePrev: false,
